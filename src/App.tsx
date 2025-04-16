53ebec639f232fe85ce1aadd989d72a2cc789ef9
gabrielagomescezar@gmail.com <gabrielagomescezar@gmail.com>
2025-04-16 18:48:51 -0300

    feat: import auth context and route

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { AuthProvider } from '../src/context/AuthContext';
+import Routes from '../src/routes';
 
 export default function App() {
   return (
-    <></>
+    <AuthProvider>
+      <Routes />
+    </AuthProvider>
   );
 }

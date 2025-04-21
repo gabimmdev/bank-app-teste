72c677a36b9da6fb6095a54ce63e5992b7fb3c2a
gabimmdev <gabrielagomescezar@gmail.com>
2025-04-21 17:01:30 -0300

    fix: navigation

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,12 +2,15 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { useAuth } from '../context/AuthContext';
+import { RootStackParamList } from './types';
 
 import LoginScreen from '../screens/LoginScreen';
 import DashboardScreen from '../screens/DashboardScreen';
 import RegisterScreen from '../screens/RegisterScreen';
+import NewTransitionScreen from '../screens/NewTransitionScreen';
+import ProfileScreen from '../screens/ProfileScreen';
 
-const Stack = createNativeStackNavigator();
+const Stack = createNativeStackNavigator<RootStackParamList>();
 
 export default function Routes() {
   const { user } = useAuth();
@@ -20,7 +23,9 @@ export default function Routes() {
   ) : (
     <>
       <Stack.Screen name="Login" component={LoginScreen} />
-      <Stack.Screen name="Register" component={RegisterScreen} />
+      <Stack.Screen name="Cadastro" component={RegisterScreen} />
+      <Stack.Screen name="Transição" component={NewTransitionScreen} />
+      <Stack.Screen name="Perfil" component={ProfileScreen}/>
     </>
   )}
 </Stack.Navigator>
---
+++ src/routes/types.ts
@@ -0,0 +1,8 @@
+export type RootStackParamList = {
+    Login: undefined;
+    Cadastro: undefined;
+    Dashboard: undefined;
+    Transição: undefined;
+    Perfil: undefined;
+  };
+  

f775cc5e7acd2e3e63273aa80cf23f0181d09b09
gabimmdev <gabrielagomescezar@gmail.com>
2025-04-22 01:21:48 -0300

    style: change color bank tittle

--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -63,7 +63,7 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 32,  
     fontWeight: 'bold', 
-    color: 'green',  
+    color: '#83a37b',  
     textAlign: 'center', 
     marginBottom: 20, 
   },

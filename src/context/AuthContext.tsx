ad5b29e2277a1f1b3b15bf0a7afa2ec99045fd99
gabimmdev <gabrielagomescezar@gmail.com>
2025-04-22 01:19:09 -0300

    fix: logout to login screen

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,5 +1,8 @@
 import React, { createContext, useState, useEffect, useContext } from 'react';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useNavigation } from '@react-navigation/native'; 
+import { RootStackParamList } from '../routes/types';
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 
 type User = {
   name: string;
@@ -23,6 +26,7 @@ const AuthContext = createContext<AuthContextType>({} as AuthContextType);
 
 export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   const [user, setUser] = useState<User | null>(null);
+  const navigation = useNavigation<NativeStackNavigationProp<RootStackParamList>>(); // Usa o hook de navegação
 
   useEffect(() => {
     const loadUser = async () => {
@@ -35,19 +39,19 @@ export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({ children
   const login = async (email: string, password: string) => {
     const storedUser = await AsyncStorage.getItem('registeredUser');
 
-  if (!storedUser) return false;
+    if (!storedUser) return false;
 
-  const parsedUser = JSON.parse(storedUser);
+    const parsedUser = JSON.parse(storedUser);
 
-  if (parsedUser.email === email && parsedUser.senha === password) {
-    const loggedUser = { name: parsedUser.name, email: parsedUser.email };
-    setUser(loggedUser);
-    await AsyncStorage.setItem('user', JSON.stringify(loggedUser));
-    return true;
-  }
+    if (parsedUser.email === email && parsedUser.senha === password) {
+      const loggedUser = { name: parsedUser.name, email: parsedUser.email };
+      setUser(loggedUser);
+      await AsyncStorage.setItem('user', JSON.stringify(loggedUser));
+      return true;
+    }
 
-  return false;
-};
+    return false;
+  };
 
   const register = async (name: string, email: string, password: string) => {
     const newUser = { name, email };
@@ -70,6 +74,7 @@ export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({ children
   const logout = async () => {
     setUser(null);
     await AsyncStorage.removeItem('user');
+    navigation.navigate('Login'); // Navega para a tela de login após o logout
   };
 
   return (
